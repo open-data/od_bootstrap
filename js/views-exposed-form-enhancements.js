91075e3d40f9d75f2c417a52d2120868544e4f88
Jeff Gordon <jeff.gordon@tbs-sct.gc.ca>
2018-07-09 11:15:15 -0400

    Fix for views exposed form enhancements js

--- js/views-exposed-form-enhancements.js
+++ js/views-exposed-form-enhancements.js
@@ -13,75 +13,77 @@
 
   Drupal.behaviors.sort_form = {
     attach: function (context, settings) {
-      if (context.constructor.name != "HTMLDocument") return;
-      $('#edit-sort-by').parents('.form-item').remove();
-      $('#sort-form-edit-sort-by').on("change", function() {
-        this.form.submit();
+      $('html', context).once('sort_form').each(function () {
+        $('#edit-sort-by').parents('.form-item').remove();
+        $('#sort-form-edit-sort-by').on("change", function() {
+          this.form.submit();
+        });
       });
     }
   };
 
   Drupal.behaviors.clear_btn = {
     attach: function (context, settings) {
-      if (context.constructor.name != "HTMLDocument") return;
-      var $textInput = $('#edit-search-api-fulltext');
+      $('html', context).once('clear_btn').each(function () {
+        var $textInput = $('#edit-search-api-fulltext');
 
-      if ($('.clear-btn').length == 0) {
-       $textInput.after('<span class="clear-btn hidden-xs" title="Clear">&times;</span>');
-      }
-      $('.clear-btn').css('visibility', ($textInput.val().length) ? "visible" : "hidden");
+        if ($('.clear-btn').length == 0) {
+         $textInput.after('<span class="clear-btn hidden-xs" title="Clear">&times;</span>');
+        }
+        $('.clear-btn').css('visibility', ($textInput.val().length) ? "visible" : "hidden");
 
-      // Show the clear button if text input value is not empty
-      $textInput.keyup(function() {
-        $('.clear-btn').css('visibility', ($(this).val().length) ? "visible" : "hidden");
-      });
+        // Show the clear button if text input value is not empty
+        $textInput.keyup(function() {
+          $('.clear-btn').css('visibility', ($(this).val().length) ? "visible" : "hidden");
+        });
 
-      // Hide the clear button on click, and reset the input value
-      $('.clear-btn').on( "click", function() {
-        $textInput.val('');
-        $(this).css('visibility', 'hidden');
-        // $(this).parents('form').find('button.form-submit').trigger('click');
+        // Hide the clear button on click, and reset the input value
+        $('.clear-btn').on( "click", function() {
+          $textInput.val('');
+          $(this).css('visibility', 'hidden');
+          // $(this).parents('form').find('button.form-submit').trigger('click');
+        });
       });
     }
   };
 
   Drupal.behaviors.filter_sort_btn = {
     attach: function (context, settings) {
-      if (context.constructor.name != "HTMLDocument") return;
+      $('html', context).once('sort_btn').each(function () {
+        var $filterElm = $('.bs-region--top-left');
+        $filterElm.attr('id', 'bs-region--top-left');
 
-      var $filterElm = $('.bs-region--top-left');
-      $filterElm.attr('id', 'bs-region--top-left');
+        var sidebarClass = "hidden-xs hidden-sm col-md-3";
+        var popupClass = "wb-overlay modal-content overlay-def wb-popup-full";
+        $('#view-filter-mobile').on( "click", function() {
+          $filterElm.removeClass(sidebarClass);
+          $filterElm.addClass(popupClass);
+          $filterElm.trigger("wb-init.wb-overlay");
+          $filterElm.trigger("open.wb-overlay");
+        });
 
-      var sidebarClass = "hidden-xs hidden-sm col-md-3";
-      var popupClass = "wb-overlay modal-content overlay-def wb-popup-full";
-      $('#view-filter-mobile').on( "click", function() {
-        $filterElm.removeClass(sidebarClass);
-        $filterElm.addClass(popupClass);
-        $filterElm.trigger("wb-init.wb-overlay");
-        $filterElm.trigger("open.wb-overlay");
-      });
-
-      // Link trigger
-      wb.doc.on( "click vclick", ".wb-overlay a[href^='#']", function( event ) {
-        $filterElm.removeClass(popupClass + " wb-overlay-inited");
-        $filterElm.addClass(sidebarClass);
-        $filterElm.find(".modal-footer,#hdrClose").remove();
-      });
-
-      // Close trigger
-      wb.doc.on( "click vclick", ".overlay-close", function( event ) {
-        $filterElm.removeClass(popupClass + " wb-overlay-inited");
-        $filterElm.addClass(sidebarClass);
-        $filterElm.find(".modal-footer,#hdrClose").remove();
-      });
-
-      // JS or keyboard trigger
-      wb.doc.on( "timerpoke.wb wb-init.wb-overlay keydown open.wb-overlay close.wb-overlay", ".wb-overlay", function( event ) {
-        if ((event.type == "close") || (event.which == 27)) {
+        // Link trigger
+        wb.doc.on( "click vclick", ".wb-overlay a[href^='#']", function( event ) {
           $filterElm.removeClass(popupClass + " wb-overlay-inited");
           $filterElm.addClass(sidebarClass);
           $filterElm.find(".modal-footer,#hdrClose").remove();
-        }
+        });
+
+        // Close trigger
+        wb.doc.on( "click vclick", ".overlay-close", function( event ) {
+          $filterElm.removeClass(popupClass + " wb-overlay-inited");
+          $filterElm.addClass(sidebarClass);
+          $filterElm.find(".modal-footer,#hdrClose").remove();
+        });
+
+        // JS or keyboard trigger
+        wb.doc.on( "timerpoke.wb wb-init.wb-overlay keydown open.wb-overlay close.wb-overlay", ".wb-overlay", function( event ) {
+          if ((event.type == "close") || (event.which == 27)) {
+            $filterElm.removeClass(popupClass + " wb-overlay-inited");
+            $filterElm.addClass(sidebarClass);
+            $filterElm.find(".modal-footer,#hdrClose").remove();
+          }
+        });
       });
     }
   };
